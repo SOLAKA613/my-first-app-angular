dbb7c80d552c5f870b3b9f8e9e2305c5a2059fbd
solaka613 <solaka613@gmail.com>
2022-10-18 00:44:10 +0200

    my first commit to my first app angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  styles: [`
+    h3 {
+        color: dodgerblue;
+      }
+  `]
 })
 export class AppComponent {
-  title = 'my-first-app';
+  username:string = "";
+  showSecret:boolean = false;
+  log: any[] = [];
+
+  onToggleDetails(){
+    this.username = '';
+    this.showSecret = !this.showSecret;
+    // this.log.push(this.log.length + 1);
+    this.log.push(new Date());
+  }
 }
